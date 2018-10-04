2c61298d67427343246b98b0c299bb05f07333d0
Tobias Pickel <tobias.pickel@xing.com>
2018-10-04 16:57:28 +0200

    refactor(index): remove example component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-/**
- * @class ExampleComponent
- */
-
-import * as React from "react";
-
 export * from "./ui";
 export * from "./components";
 export * from "./layout";
@@ -11,13 +5,3 @@ export * from "./forms";
 
 export { injectGlobal } from "react-emotion";
 export { ThemeProvider } from "emotion-theming";
-
-export type Props = { text: string };
-
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const { text } = this.props;
-
-    return <div>Example Component: {text}</div>;
-  }
-}
