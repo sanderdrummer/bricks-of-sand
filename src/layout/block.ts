d33a0a03445f8729e89cd4c438b3283de293f9ba
Tobias Pickel <tobias.pickel@xing.com>
2018-10-04 17:42:24 +0200

    fix(block): props should be optional

--- src/layout/block.ts
+++ src/layout/block.ts
@@ -1,10 +1,10 @@
 import styled from "react-emotion";
 
 export interface BlockProps {
-  height: string;
-  margin: string;
-  padding: string;
-  width: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  width?: string;
 }
 export const Block = styled("div")<BlockProps>({}, props => ({
   height: props.height,
