2b48af3de86a108aa0c4cb7fc4ae3f4e81f04688
Tobias Pickel <tobias.pickel@xing.com>
2018-10-19 16:45:26 +0200

    feat(from-group): add support for radio and checkbox

--- src/forms/form-group.ts
+++ src/forms/form-group.ts
@@ -1,5 +1,11 @@
 import styled from "react-emotion";
 
 export const FormField = styled("div")({
+  "input[type=checkbox]": {
+    marginLeft: "0.5rem"
+  },
+  "input[type=radio]": {
+    marginLeft: "0.5rem"
+  },
   marginBottom: "0.5rem"
 });
