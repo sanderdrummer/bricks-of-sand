a87f5edcf826c29988964ee8fad7ec0a8fd615d4
Tobias Pickel <tobias.pickel@xing.com>
2018-10-19 16:45:09 +0200

    refactor(material-input): extends block

--- src/forms/material-inputs.ts
+++ src/forms/material-inputs.ts
@@ -1,12 +1,16 @@
 import styled from "react-emotion";
+import { Block } from "../layout/block";
 import { shadows } from "../ui/theme";
 
-export const MaterialInput = styled("div")({
-  background: "white",
-  input: {
-    border: "none",
-    boxShadow: shadows.level5,
-    padding: "0.8rem 0.5rem"
+export const MaterialInput = styled(Block)(
+  {
+    input: {
+      border: "none",
+      boxShadow: shadows.level5,
+      padding: "0.8rem 0.5rem"
+    }
   },
-  margin: "3rem"
-});
+  props => ({
+    background: props.theme.white
+  })
+);
