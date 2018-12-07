2a18e5de57b58d7a962e737789ee3130a1c9f22d
Tobias Pickel <tobias.pickel@xing.com>
2018-12-07 16:19:59 +0100

    feat(typography): add components

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ export * from "./layout";
 export * from "./forms";
 export * from "./theme";
 export * from "./icons";
+export * from "./typography";
 
 export { injectGlobal } from "react-emotion";
 export { withTheme } from "emotion-theming";
---
+++ src/typography.ts
@@ -0,0 +1,37 @@
+import { withTheme } from "emotion-theming";
+import styled from "react-emotion";
+
+export const CenterText = styled("span")({
+  textAlign: "center"
+});
+
+interface AlertTextProps {
+  value: number;
+}
+export const AlertText = withTheme(
+  styled("span")<AlertTextProps>({}, props => ({
+    color: props.value < 0 ? props.theme.red : undefined
+  }))
+);
+
+interface LineThroughProps {
+  lineThrough?: boolean;
+}
+
+export const LineThrough = styled("div")<LineThroughProps>({}, props => ({
+  textDecoration: props.lineThrough ? "line-through" : "none"
+}));
+
+interface TextAlignProps {
+  textAlign: any;
+}
+export const TextAlign = styled("div")<TextAlignProps>({}, props => ({
+  textAlign: props.textAlign
+}));
+
+export const Ellipsis = styled("div")({
+  minWidth: 0,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap"
+});
