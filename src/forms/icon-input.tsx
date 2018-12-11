dc57a2c9c80d314f7def7fe8a7e086c4dcd68681
Tobias Pickel <tobias.pickel@xing.com>
2018-12-11 08:52:36 +0100

    fix(icon-input): fix color of svg icon

--- src/forms/icon-input.tsx
+++ src/forms/icon-input.tsx
@@ -12,18 +12,25 @@ export const IconInput = withTheme(
     input: {
       background: "transparent",
       borderBottom: `solid 1px transparent`,
+      color: theme.primary,
       padding: "0 0 0.2rem 0.2rem",
       transition: "100ms ease-in-out",
       width: inactiveWidth
     },
     ["input:focus"]: {
+      "&+svg": {
+        fill: theme.primary + "!important"
+      },
       borderBottom: `solid 1px ${theme.primary}`,
       placeholder: theme.primary,
       width: activeWidth
     },
+    ["input::placeholder"]: {
+      color: theme.textLight
+    },
     position: "relative",
     svg: {
-      fill: theme.primary,
+      fill: theme.textLight + "!important",
       position: "absolute",
       right: 0
     }
