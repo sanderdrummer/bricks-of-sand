da5ff8a2a6ee34e5ab17e8a30acd4c19d7ab337b
Tobias Pickel <tobias.pickel@xing.com>
2018-12-11 08:49:54 +0100

    fix(input): align stylings for placeholder color

--- src/forms/input.ts
+++ src/forms/input.ts
@@ -4,10 +4,13 @@ export const Input = styled("input")(props => ({
   background: props.theme.componentBackgroundDark,
   border: "solid 1px transparent",
   borderRadius: props.theme.borderRadius,
-  color: props.theme.text,
+  color: props.theme.primary,
   padding: "0.5rem",
   width: "100%",
   ["&:focus"]: {
     boxShadow: props.theme.inputShadow
+  },
+  ["input::placeholder"]: {
+    color: props.theme.textLight
   }
 }));
